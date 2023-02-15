cbf827c7373f4c251c33cc99d4184939e7e10693
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-15 14:44:57 -0300

    feat: Praticando variáveis do tipo inteiras

---
+++ CSharp-OOP/2-VariablesInt/Program.cs
@@ -0,0 +1,13 @@
+﻿class VariablesInt
+{
+    static void Main(string[] args)
+    {
+        int age = 22;
+        Console.WriteLine(age);
+        Console.WriteLine("Esta é minha idade: " + age + " anos.");
+        Console.WriteLine(string.Format("Minha idade é {0} anos.", age));
+        //Dscomente o trecho a baixo caso execute o arquivo "buildado"
+        //Console.WriteLine("Precione Enter para finalizar...");
+        //Console.readline();
+    }
+}
