8f067696530c142866b2f1dc52771e1d754f39f6
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-15 18:22:46 -0300

    feat: treinando o tipo string

--- CSharp-Explore/5-VariablesChar/Program.cs
+++ CSharp-Explore/5-VariablesChar/Program.cs
@@ -2,8 +2,9 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Estuando varia veis do tipo 'char'");
-
+        Console.WriteLine("Estuando variaveis do tipo 'char'");
+        // Armazenão apenas um caracter
+     
         // Observer que o caracter cgar usa aspas simplesm diferente do writeline
         // Aspas duoplas representão texto
         // Aspas simples, o caracter char
@@ -16,6 +17,20 @@
 
         letra = (char)(65 + 1);
         Console.WriteLine(letra);
+
+        Console.WriteLine("Estuando variaveis do tipo 'string'");
+
+        string frase = "Estudando varios tipos de C#";
+        Console.WriteLine(frase);
+
+        string cursos = @"
+Devo estudar:
+ - C#
+ - Java
+ - Typescript(aprofundar)
+ - Python(aprofundar)
+ - Etc.";
+        Console.WriteLine(cursos);
 
         //Dscomente o trecho a baixo caso execute o arquivo "buildado"
         //Console.WriteLine("Precione Enter para finalizar...");
