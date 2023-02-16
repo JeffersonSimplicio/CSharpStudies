01ed847e13d9ca256e9d7830120d177cffda7131
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-16 16:14:39 -0300

    feat: usado for e foreach

--- CSharp-Explore/7-Laços de repetição/Program.cs
+++ CSharp-Explore/7-Laços de repetição/Program.cs
@@ -11,7 +11,7 @@
         double investimento = 1000;
         int meses = 10;
         int controle = 0;
-        while (controle < meses)
+        while (controle < meses) // condiçao de parada
         {
             controle++;
             investimento *= 1.05;
@@ -33,6 +33,21 @@
         Console.WriteLine(investimento);
 
         Console.WriteLine("For - Foreach");
+
+        Console.WriteLine("- For");
+
+        for (int i = 0; i < 5; i++)//inicio/ fim/ passo
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("- Foreach");
+
+        string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        foreach (string car in cars)
+        {
+            Console.WriteLine(car);
+        }
         //Dscomente o trecho a baixo caso execute o arquivo "buildado"
         //Console.WriteLine("Precione Enter para finalizar...");
         //Console.readline();
