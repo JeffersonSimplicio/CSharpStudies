72f161c165ed4f1d85eac15d4fd3db1ed799770b
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-16 19:23:35 -0300

    feat: adiciona método Deposito

--- CSharp-OOP/Bytebank/ContaCorrente.cs
+++ CSharp-OOP/Bytebank/ContaCorrente.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//namespace Bytebank
+//{
+//    public class ContaCorrente
+//    {
+//        public string titular;
+//        public string conta;
+//        public int agencia;
+//        public double saldo;
+//    }
+//}
+
+//Usando "constructor"
 namespace Bytebank
 {
     public class ContaCorrente
@@ -12,5 +24,18 @@ namespace Bytebank
         public string conta;
         public int agencia;
         public double saldo;
+
+        public ContaCorrente(string titular, string conta, int agencia, double saldo)
+        {
+            this.titular = titular;
+            this.conta = conta;
+            this.agencia = agencia;
+            this.saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            this.saldo += valor;
+        }
     }
 }
--- CSharp-OOP/Bytebank/Program.cs
+++ CSharp-OOP/Bytebank/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Bytebank;
-ContaCorrente contaJefferson =  new ContaCorrente();
-contaJefferson.titular = "Jefferson Simplicio";
-contaJefferson.conta = "1010-1";
-contaJefferson.agencia = 1;
-contaJefferson.saldo = 200;
+ContaCorrente contaJefferson =  new ContaCorrente(
+    "Jefferson Simplicio",
+    "1010 - 1",
+    1,
+    200
+);
+//contaJefferson.titular = "Jefferson Simplicio";
+//contaJefferson.conta = "1010-1";
+//contaJefferson.agencia = 1;
+//contaJefferson.saldo = 200;
 
 Console.WriteLine(contaJefferson.titular);
+Console.WriteLine(contaJefferson.saldo);
+contaJefferson.Deposito(100);
+Console.WriteLine(contaJefferson.saldo);
