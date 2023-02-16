c03d2cb4a96c80223b2ac6f681016537055055be
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-16 18:57:50 -0300

    feat: criada primeira classe em C#

---
+++ CSharp-OOP/Bytebank/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int agencia;
+        public double saldo;
+    }
+}
---
+++ CSharp-OOP/Bytebank/Program.cs
@@ -0,0 +1,8 @@
+﻿using Bytebank;
+ContaCorrente contaJefferson =  new ContaCorrente();
+contaJefferson.titular = "Jefferson Simplicio";
+contaJefferson.conta = "1010-1";
+contaJefferson.agencia = 1;
+contaJefferson.saldo = 200;
+
+Console.WriteLine(contaJefferson.titular);
