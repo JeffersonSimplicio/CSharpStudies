3cd4a18cc6bea8cf2761bdb5fec901a9254ba972
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-17 12:37:37 -0300

    feat: inicia etapa de segurança

--- CSharp-OOP/Bytebank/Program.cs
+++ CSharp-OOP/Bytebank/Program.cs
@@ -6,29 +6,12 @@ Titular clinteJefferson = new Titular(
     "123.456.789-10",
     "Web Developer"
 );
-ContaCorrente contaJefferson =  new ContaCorrente(
+ContaCorrente contaJefferson = new ContaCorrente(
     clinteJefferson,
     "1010-1",
     1,
     200
 );
-//contaJefferson.titular = "Jefferson Simplicio";
-//contaJefferson.conta = "1010-1";
-//contaJefferson.agencia = 1;
-//contaJefferson.saldo = 200;
-
-Console.WriteLine("Titular: " + contaJefferson.titular);
-Console.WriteLine();
-Console.WriteLine("saldo pre-deposito: " + contaJefferson.saldo);
-contaJefferson.Depositar(100);
-Console.WriteLine("saldo pos-deposito: " + contaJefferson.saldo);
-Console.WriteLine();
-Console.WriteLine("saldo pre-saque: " + contaJefferson.saldo);
-bool check = contaJefferson.Sacar(100);
-string response = check ? "Saque bem sucedido" : "Não possui saldo suficiente";
-Console.WriteLine(response);
-Console.WriteLine("saldo pos-saque: " + contaJefferson.saldo);
-Console.WriteLine();
 
 Titular clinteAna = new Titular(
     "Ana Leticia",
@@ -41,11 +24,24 @@ ContaCorrente contaAna = new ContaCorrente(
     1,
     250
 );
-Console.WriteLine("saldo pre-transferencia - Ana: " + contaAna.saldo);
-Console.WriteLine("saldo pre-transferencia - Jeffeson: " + contaJefferson.saldo);
-bool check2 = contaAna.Transferir(25, contaJefferson);
-string response2 = check2 ? "Transferencia bem sucedida" : "Não possui saldo suficiente";
-Console.WriteLine(response2);
-Console.WriteLine("saldo pos-transferencia - Ana: " + contaAna.saldo);
-Console.WriteLine("saldo pos-transferencia - Jeffeson: " + contaJefferson.saldo);
-contaJefferson.ExibirDadosDaConta();
+
+//Console.WriteLine("Titular: " + contaJefferson.titular);
+//Console.WriteLine();
+//Console.WriteLine("saldo pre-deposito: " + contaJefferson.saldo);
+//contaJefferson.Depositar(100);
+//Console.WriteLine("saldo pos-deposito: " + contaJefferson.saldo);
+//Console.WriteLine();
+//Console.WriteLine("saldo pre-saque: " + contaJefferson.saldo);
+//bool check = contaJefferson.Sacar(100);
+//string response = check ? "Saque bem sucedido" : "Não possui saldo suficiente";
+//Console.WriteLine(response);
+//Console.WriteLine("saldo pos-saque: " + contaJefferson.saldo);
+//Console.WriteLine();
+//Console.WriteLine("saldo pre-transferencia - Ana: " + contaAna.saldo);
+//Console.WriteLine("saldo pre-transferencia - Jeffeson: " + contaJefferson.saldo);
+//bool check2 = contaAna.Transferir(25, contaJefferson);
+//string response2 = check2 ? "Transferencia bem sucedida" : "Não possui saldo suficiente";
+//Console.WriteLine(response2);
+//Console.WriteLine("saldo pos-transferencia - Ana: " + contaAna.saldo);
+//Console.WriteLine("saldo pos-transferencia - Jeffeson: " + contaJefferson.saldo);
+//contaJefferson.ExibirDadosDaConta();
