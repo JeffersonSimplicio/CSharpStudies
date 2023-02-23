586398cf3f798e1296b90eb0b147567f3d5097f4
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-23 11:56:23 -0300

    feat: cria classe Diretor

--- CSharp-Inheritance_Interface/Bytebank_ADM/Program.cs
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Bytebank_ADM.Funcionarios;
 
-Funcionaio pedro = new Funcionaio("Pedro", "12345678901", 1234.56);
+Funcionario pedro = new Funcionario("Pedro", "12345678901", 1234.56);
 Console.WriteLine(pedro.Nome);
 Console.WriteLine(pedro.Bonificacao());
+
+Diretor roberta = new Diretor("Roberta", "98765432101", 4321.09);
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.Bonificacao());
