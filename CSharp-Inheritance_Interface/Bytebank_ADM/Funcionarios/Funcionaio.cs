0000f6d948f8a7cd028a1611e03c561a1b8f212c
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-23 11:47:12 -0300

    feat: cria a classe Funcionario

---
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Funcionarios/Funcionaio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank_ADM.Funcionarios
+{
+    public class Funcionaio
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionaio(string nome, string cpf, double salario)
+        {
+            this.Nome = nome;
+            this.CPF = cpf;
+            this.Salario = salario;
+        }
+
+        public string Bonificacao()
+        {
+            return String.Format("{0:N}", this.Salario * 0.1);
+        }
+    }
+}
---
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Program.cs
@@ -0,0 +1,5 @@
+﻿using Bytebank_ADM.Funcionarios;
+
+Funcionaio pedro = new Funcionaio("Pedro", "12345678901", 1234.56);
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.Bonificacao());
