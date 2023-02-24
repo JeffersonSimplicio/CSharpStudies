b5dd4a63dbaa98de097f0aa82f705a4334da2910
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-24 15:20:36 -0300

    feat: criação da classe Designer

---
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Funcionarios/Designer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank_ADM.Funcionarios
+{
+    public class Designer : Funcionario
+    {
+        public Designer(string nome, string cpf, double salario) : base(nome, cpf, salario)
+        {
+        }
+        //base - Equivalente ao super
+
+        public override double Bonificacao()
+        {
+            return this.Salario * 0.17 + base.Bonificacao();
+        }
+
+        public override void aumentoSalario()
+        {
+            this.Salario *= 1.11;
+        }
+    }
+}
