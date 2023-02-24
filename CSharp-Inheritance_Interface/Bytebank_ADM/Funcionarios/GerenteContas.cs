1debac6957cf805710b0b560ebe62360caebc1d6
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-24 15:27:22 -0300

    feat: cria classe GerenteContas

---
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Funcionarios/GerenteContas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank_ADM.Funcionarios
+{
+    public class GerenteContas : Funcionario
+    {
+        static double salarioPadrao = 4000;
+        public GerenteContas(string nome, string cpf) : base(nome, cpf, salarioPadrao)
+        {
+        }
+        //base - Equivalente ao super
+
+        public override double Bonificacao()
+        {
+            return this.Salario * 0.25 + base.Bonificacao();
+        }
+
+        public override void aumentoSalario()
+        {
+            this.Salario *= 1.05;
+        }
+    }
+}
