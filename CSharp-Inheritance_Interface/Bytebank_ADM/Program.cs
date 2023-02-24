12ba4ad54ddf1845d20167d55565d7e28c1626db
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-24 15:50:34 -0300

    feat: log personalizado

--- CSharp-Inheritance_Interface/Bytebank_ADM/Program.cs
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Program.cs
@@ -2,19 +2,10 @@
 using Bytebank_ADM.Utilidades;
 
 Funcionario pedro = new Funcionario("Pedro", "12345678901", 1234.56);
-Console.WriteLine(
-    string.Format(
-        "{0}: salário de R${1:N}; bonificação de R${2:N}", pedro.Nome, pedro.Salario, pedro.Bonificacao()
-    )
-);
+Log.Funcionario(pedro);
 
 Diretor roberta = new Diretor("Roberta", "98765432101");
 Log.Funcionario(roberta);
-//Console.WriteLine(
-//    string.Format(
-//        "{0}: salário de R${1:N}; bonificação de R${2:N}", roberta.Nome, roberta.Salario, roberta.Bonificacao()
-//    )
-//);
 
 Console.WriteLine("Total de funcionaios: " + Funcionario.quantidadeFuncionarios);
 
