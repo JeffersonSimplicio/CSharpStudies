58b8648d9a89861e24cc8c6fbc92390b863d9043
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-24 15:30:15 -0300

    feat: criada classe Auxiliar

---
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Funcionarios/Auxiliar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank_ADM.Funcionarios
+{
+    public class Auxiliar : Funcionario
+    {
+        static double salarioPadrão = 2000;
+        public Auxiliar(string nome, string cpf) : base(nome, cpf, salarioPadrão)
+        {
+        }
+        //base - Equivalente ao super
+
+        public override double Bonificacao()
+        {
+            return this.Salario * 0.2 + base.Bonificacao();
+        }
+
+        public override void aumentoSalario()
+        {
+            this.Salario *= 1.1;
+        }
+    }
+}
