b16cd46b2d94ff381da4b27f81349399c1d4c129
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-25 18:44:38 -0300

    feat: cria ParceiroComercial

---
+++ CSharp-Inheritance_Interface/Bytebank_ADM/Parceiro/ParceiroComercial.cs
@@ -0,0 +1,26 @@
+﻿using Bytebank_ADM.SistemaInterno;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank_ADM.Parceiro
+{
+    public class ParceiroComercial : IAutenticavel
+    {
+        public ParceiroComercial(string nome, string senha)
+        {
+            this.Nome = nome;
+            this.Senha = senha;
+        }
+
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+
+        public bool Autenticar(string senha)
+        {
+            return this.Senha == senha;
+        }
+    }
+}
