d8494547042ac25fe768b725e5dbc9e721a19492
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-07 19:31:03 -0300

    feat: create class ContaCorrente

---
+++ CSharp-Exception/Bytebank/Contas/ContaCorrente.cs
@@ -0,0 +1,92 @@
+﻿using Bytebank.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Contas
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numero_agencia, string numero_conta, Titular cliente)
+        {
+            if (numero_agencia <= 0)
+            {
+                throw new ArgumentException("Número de agência menor ou igual a zero!", nameof(numero_agencia));
+            }
+            this.Numero_agencia = numero_agencia;
+            this.Conta = numero_conta;
+            this.Cliente = cliente;
+
+            /*
+            try
+            {
+                TaxaOperacao = 30 / TotalDeContasCriadas;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ocorreu um erro! Não é possível fazer uma divisão por zero!");
+            }          
+            */
+            TotalDeContasCriadas++;
+        }
+
+        public static int TotalDeContasCriadas { get; private set; }
+        public static float TaxaOperacao { get; private set; }
+
+        public string Conta { get; set; }
+        public Titular Cliente { get; set; }
+        private int _numero_agencia;
+        public int Numero_agencia
+        {
+            get { return this._numero_agencia; }
+            private set
+            {
+                if (value > 0)
+                {
+                    this._numero_agencia = value;
+                }
+            }
+        }
+
+        private double _saldo = 100;
+        public double GetSaldo()
+        {
+            return this._saldo;
+        }
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this._saldo)
+            {
+                this._saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            Sacar(valor);
+            destino.Depositar(valor);
+            return true;
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if (valor > 0)
+            {
+                this._saldo = valor;
+            }
+        }
+    }
+}
