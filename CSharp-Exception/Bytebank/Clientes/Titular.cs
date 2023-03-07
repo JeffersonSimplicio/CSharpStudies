8833e2b8e3bb13e885d9bbf82bde47782d3f6dfa
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-07 18:09:14 -0300

    create class Titular

---
+++ CSharp-Exception/Bytebank/Clientes/Titular.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Clientes
+{
+    public class Titular
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+
+        public Titular(string nome,
+                       string cpf)
+        {
+            this.Cpf = cpf;
+            this.Nome = nome;
+        }
+    }
+}
