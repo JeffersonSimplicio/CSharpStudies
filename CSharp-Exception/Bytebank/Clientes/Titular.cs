7cf1c935aa4380333f7aec1024685d630a8c9a40
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-07 18:25:29 -0300

    feat: add profisao

--- CSharp-Exception/Bytebank/Clientes/Titular.cs
+++ CSharp-Exception/Bytebank/Clientes/Titular.cs
@@ -10,10 +10,9 @@ namespace Bytebank.Clientes
     {
         public string Nome { get; set; }
         public string Cpf { get; set; }
-        public string Profissao { get; set; }
+        public string? Profissao { get; set; }
 
-        public Titular(string nome,
-                       string cpf)
+        public Titular(string nome, string cpf)
         {
             this.Cpf = cpf;
             this.Nome = nome;
