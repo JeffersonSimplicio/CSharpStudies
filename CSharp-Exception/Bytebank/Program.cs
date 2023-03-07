771aae46f7cc185a532af976ca14e45dbdb282a4
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-07 19:31:26 -0300

    feat: create Main

---
+++ CSharp-Exception/Bytebank/Program.cs
@@ -0,0 +1,15 @@
+﻿using Bytebank.Contas;
+using Bytebank.Clientes;
+ 
+
+try
+{
+    Titular Iasmin = new Titular("Iasmin Ferreira", "12345678910");
+    ContaCorrente conta1 = new ContaCorrente(0, "1234-X", Iasmin);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Parâmetro com erro: " + ex.ParamName);
+    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+    Console.WriteLine(ex.Message);
+}
