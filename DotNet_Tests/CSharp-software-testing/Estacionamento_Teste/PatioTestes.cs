eebe64203684c53e52b5fe0686cd44368bde7209
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-09 09:49:33 -0300

    test: create FaturamentoTeste

---
+++ DotNet_Tests/CSharp-software-testing/Estacionamento_Teste/PatioTestes.cs
@@ -0,0 +1,34 @@
+﻿using Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento_Teste
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void FaturamentoTeste()
+        {
+            //Arrange
+            Patio estacionamento = new Patio();
+
+            Veiculo carro = new Veiculo();
+            carro.Proprietario = "Jefferson";
+            carro.Placa = "abc-1234";
+            carro.Modelo = "Belina";
+            carro.Cor = "Branco";
+
+            estacionamento.RegistrarEntradaVeiculo(carro);
+            estacionamento.RegistrarSaidaVeiculo("abc-1234");
+
+            //Act
+            double faturado = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturado);
+        }
+    }
+}
